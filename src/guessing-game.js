ead25a0ae3558bb3d1efe5f753365330c077371d
Olena Iarmusheva <Olena.Iarmusheva@teaminternational.com>
2019-10-09 14:57:53 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = null;
+        this._max = null;
+        this._t = null;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._t = Math.ceil((this._min + this._max) / 2);
+        return this._t;
     }
 
     lower() {
-
+        this._max= this._t;
     }
 
     greater() {
-
+        this._min = this._t;
     }
+
 }
 
 module.exports = GuessingGame;
